373750c666a6a38ac169e2928c5f84d462cb85e8
Simtel <simuls@gmail.com>
2025-04-01 09:22:27 +0400

    refactor get by year

--- internal/adapter/sqliterepo/zenrepo/transactions/get_by_year.go
+++ internal/adapter/sqliterepo/zenrepo/transactions/get_by_year.go
@@ -1,6 +1,7 @@
 package transactions
 
 import (
+	"log"
 	"money-stat/internal/model"
 	"time"
 )
@@ -9,5 +10,20 @@ func (r *Repository) GetByYear(year int) []model.Transaction {
 	firstDay := time.Date(year, time.January, 1, 0, 0, 0, 0, time.UTC)
 	lastDay := time.Date(year, time.December, 31, 23, 59, 59, 59, time.UTC)
 
-	return r.GetBetweenDate(firstDay, lastDay)
+	db := r.db
+
+	var transactions []model.Transaction
+	err := db.Model(&model.Transaction{}).
+		Where(
+			"date BETWEEN ? and ? AND deleted = ?",
+			firstDay.Format("2006-01-02"),
+			lastDay.Format("2006-01-02"),
+			0).
+		Order("date ASC").
+		Find(&transactions).
+		Error
+	if err != nil {
+		log.Println(err)
+	}
+	return transactions
 }
