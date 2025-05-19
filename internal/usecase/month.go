9e424493d5bca1ab70a74bc897ab56e0bbef51e1
Simtel <simuls@gmail.com>
2025-05-19 10:22:04 +0400

    refactor months

--- internal/usecase/month.go
+++ internal/usecase/month.go
@@ -51,36 +51,14 @@ func (m *Month) GetMonthStat(month string) MonthStatDto {
 	for _, transaction := range transactions {
 		cnt++
 
-		var transactionTags string
-		for _, tag := range transaction.Tag {
-			transactionTags += tag.Title + " "
-		}
-
-		if transactionTags == "" {
-			transactionTags = "Перевод"
-		}
-
-		var account string
-		if transaction.IsIncome() {
-			account = transaction.InAccount.Title
-		}
-
-		if transaction.IsOutcome() {
-			account = transaction.OutAccount.Title
-		}
-
-		if transaction.IsTransfer() {
-			account = transaction.OutAccount.Title + "->" + transaction.InAccount.Title
-		}
-
 		tCreatedDate := time.Unix(transaction.Created, 0)
 		monthStat.Transactions = append(
 			monthStat.Transactions,
 			MonthStatTransactionDto{
 				transaction.Date,
-				transactionTags,
+				m.getTags(transaction),
 				transaction.FormatAmount(),
-				account,
+				m.getAccountTitle(transaction),
 				tCreatedDate.Format("2006-01-02 15:04:05"),
 			},
 		)
@@ -99,4 +77,32 @@ func (m *Month) GetMonthStat(month string) MonthStatDto {
 
 	return monthStat
 
+}
+
+func (m *Month) getAccountTitle(transaction model.Transaction) string {
+	var account string
+	if transaction.IsIncome() {
+		account = transaction.InAccount.Title
+	}
+
+	if transaction.IsOutcome() {
+		account = transaction.OutAccount.Title
+	}
+
+	if transaction.IsTransfer() {
+		account = transaction.OutAccount.Title + "->" + transaction.InAccount.Title
+	}
+	return account
+}
+
+func (m *Month) getTags(transaction model.Transaction) string {
+	var transactionTags string
+	for _, tag := range transaction.Tag {
+		transactionTags += tag.Title + " "
+	}
+
+	if transactionTags == "" {
+		transactionTags = "Перевод"
+	}
+	return transactionTags
 }
