45fcbdfde308d16a423caef79e21e20ffb0cd5ea
Simtel <simuls@gmail.com>
2026-05-18 09:45:45 +0400

    fix tests

--- internal/adapter/sqliterepo/zenrepo/transactions/get_all.go
+++ internal/adapter/sqliterepo/zenrepo/transactions/get_all.go
@@ -6,11 +6,7 @@ func (r *Repository) GetAll() ([]model.Transaction, error) {
 	db := r.db
 	var transactions []model.Transaction
 	err := db.Model(&model.Transaction{}).
-		Where(" deleted = ?", 0).
-		Joins("InAccount").
-		Joins("OutAccount").
-		Joins("InAccount.Currency").
-		Joins("OutAccount.Currency").
+		Where("deleted = ?", 0).
 		Order("date ASC").
 		Find(&transactions).
 		Error
--- internal/adapter/sqliterepo/zenrepo/transactions/get_beetween_date.go
+++ internal/adapter/sqliterepo/zenrepo/transactions/get_beetween_date.go
@@ -13,9 +13,6 @@ func (r *Repository) GetBetweenDate(first time.Time, last time.Time) []model.Tra
 	var transactions []model.Transaction
 	err := db.Model(&model.Transaction{}).
 		Where("date BETWEEN ? and ? AND deleted = ?", first.Format("2006-01-02"), last.Format("2006-01-02"), 0).
-		Preload("Tag").
-		Joins("InAccount").
-		Joins("OutAccount").
 		Order("date ASC").
 		Find(&transactions).
 		Error
